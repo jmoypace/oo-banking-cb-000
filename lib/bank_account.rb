66934770cab3b57fcd48c8e2760baa4596585537
jmoypace <jmoypace@gmail.com>
2018-07-18 01:38:27 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,13 @@
 class BankAccount
-  attr_accessor  :balance, :status
   attr_reader :name
+  attr_accessor :balance, :status
 
-def initialize(name)
-@name=name
-@balance=1000
-@status="open"
-end
-
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
 def deposit
 balance+=1000
 end
